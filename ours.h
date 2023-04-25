2d46d52bba1606942f1d918d228cf0cee843a410
Nothando <kimndex@gmail.com>
2023-04-25 08:38:56 -0700

    environment tasks

--- ours.h
+++ ours.h
@@ -3,4 +3,11 @@
 #include <string.h>
 
 /* to change current directory task 10 */
-int _mycd(info_t *);
+int _ourcd(info_t *);
+
+/* for the environment tasks 5 and 9 */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
