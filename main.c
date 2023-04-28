2fe85a8f92d2367edf21214e959ad7d37370076b
Nothando <kimndex@gmail.com>
2023-04-28 05:25:08 -0700

    fixing bugs

--- main.c
+++ main.c
@@ -3,16 +3,12 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "(OurShell) ~ ";
-	char *lineptr = NULL, *lineptr_copy = NULL;
+	char *lineptr;
 	size_t n = 0;
 	ssize_t xread_chars;
-	const char *delim = " \n"
-	int num_tokens = 0;
-	char *token;
-	int j;
 
 	/* void variables declaration */
-	(void)ac;
+	(void)ac;(void)argv;
 
 	/* creating an infinite loop */
 	while (1){
@@ -23,24 +19,13 @@ int main(int ac, char **argv)
 	if (xread_chars == -1){
 		printf("Leaving shell..\n");
 		return (-1);
-	}
-
-	/*space allocation for lineptr copy*/
-	lineptr_copy = malloc(sizeof(char) * xread_chars);
-        if (lineptr_copy== NULL){
-            perror("tsh: memory allocation error");
-            return (-1);
-        }
-	/* copy lineptr to lineptr_copy */
-	strcpy(lineptr_copy, lineptr);
 
 	printf("%s\n", lineptr);
-
-
 
 	/*freeing up allocated memory */
 	free(lineptr);
 	}
 
 	return (0);
+}
 }
